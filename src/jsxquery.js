e525586bf623e7dbeba25015d8730dcc0b6b989d
Justin Silvestre <justinsilvestre@gmail.com>
2016-04-20 12:13:46 -0500

    Clean up entry point, webpack config, and package.json in preparation for publication

--- src/jsxquery.js
+++ src/jsxquery.js
@@ -2,14 +2,14 @@ import Element, { createElement } from './classes/Element';
 import Component, { createClass } from './classes/Component';
 import classNames from './classNames';
 import ConditionalValue from './classes/ConditionalValue';
+import babelTransformNamespacedNames from './babelTransformPlugins/namespacedNames';
+import babelTransformConditionalExpressions from './babelTransformPlugins/conditionalExpressions';
 
 const jsxQuery = {
   Element,
-
   createElement,
-
+  
   Component,
-
   createClass,
 
   ternary(test, consequent, alternate) {
@@ -18,31 +18,8 @@ const jsxQuery = {
 
   classNames,
 
-  // map(collection, callbackParam, callback) {
-  //   const el = new Element('c:forEach',
-  //     { items: collection, 'var': callbackParam },
-  //     callback(callbackParam));
-  //   // all the dot operators in the callback will
-  //   // be translated into string concatenation?
-  //   // there might be some logic about like styles or something in here
-  //   // but that will be transformed into jQuery elsewhere
-  //   return el;
-  // },
-
-  // cIf(condition, consequent) {
-  //   const el = new Element('c:if', { test: '${' + condition + '}' }, consequent);
-  //   return el.html();
-  // },
-
-  // doubleAnd(condition, ifTrue) {
-  //   return jsxQuery.cIf(condition, ifTrue);
-  // },
-
-  // doubleOr(condition, ifFalse) {
-  //   // maybe not like this, because in JS
-  //   // this would return condition if true (like, might be safe assignment)
-  //   return jsxQuery.cIf(`!(${condition})`, ifFalse);
-  // },
+  babelTransformNamespacedNames,
+  babelTransformConditionalExpressions,
 };
 
 module.exports = jsxQuery;
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,4 +1,5 @@
 var c = require('./webpack.config');
 c.entry = ['./src/jsxquery.js'];
+c.output.libraryTarget = 'commonjs2';
 
 module.exports = c;
