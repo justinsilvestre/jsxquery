7e7816c487759a8120c9b0342f4a5e6373caf06b
Justin Silvestre <justinsilvestre@gmail.com>
2016-05-13 15:33:24 -0500

    Clean up condition expressions transform

--- src/babelTransformPlugins/conditionalExpressions.js
+++ src/babelTransformPlugins/conditionalExpressions.js
@@ -1,6 +1,6 @@
 function isCreateElementCall(path) {
   return path.isCallExpression() &&
-    ((path.node.callee.type ==='Identifier' && path.node.callee.name === 'createElement')
+    ((path.node.callee.type === 'Identifier' && path.node.callee.name === 'createElement')
     || path.node.callee.type === 'MemberExpression' && path.node.callee.property.name === 'createElement');
 }
 
@@ -10,7 +10,7 @@ function isWithinCreateElementCall(path) {
 
   if (isCreateElementCall(parentPath))
     return true;
-  
+
   return isWithinCreateElementCall(parentPath);
 }
 
@@ -21,46 +21,14 @@ module.exports = function(b) {
         if(!isWithinCreateElementCall(conditionalExpressionPath))
           return;
 
-            var identifiersExceptPropertyNames = [];
-            conditionalExpressionPath.traverse({
-              Identifier(identifierPath) {
-                if (identifierPath.node.end > conditionalExpressionPath.node.test.end || identifierPath.node === identifierPath.parent.property)
-                  return;
-                identifiersExceptPropertyNames.push(JSON.stringify(identifierPath.node.name));
-              },
-            });
-            const commaJoinedIdentifiers = identifiersExceptPropertyNames.join(', ');
+        const funcTemplate = b.template(`jsxQuery.ternary(TEST, CONSEQUENT, ALTERNATE)`);
 
-            const funcTemplate = b.template(`
-              jsxQuery.ternary(TEST, CONSEQUENT, ALTERNATE)
-              `);
-
-        conditionalExpressionPath.replaceWith
-           (funcTemplate
-            ({
-              TEST: conditionalExpressionPath.node.test,
-              CONSEQUENT: conditionalExpressionPath.node.consequent,
-              ALTERNATE: conditionalExpressionPath.node.alternate,
-            })
-          );
-
-
-            // // // SHOULD ONLY REPLACE WITHIN CREATEELEMENT CALLS.
-            // // // or rather, should only replace when either the consequent, alternate,
-            // // // or entire expression is in a createelement call's arguments??
-            // // // ok: either the ternary expression is right int he createelemnt call
-            // // // or it was assigned to a variable that is in that place.
-
-            // conditionalExpressionPath.replaceWith(
-            //   funcTemplate({
-            //     TEST: conditionalExpressionPath.node.test,
-            //     CONSEQUENT: conditionalExpressionPath.node.consequent,
-            //     ALTERNATE: conditionalExpressionPath.node.alternate,
-            //   })
-            // );
-
-
-      }
+        conditionalExpressionPath.replaceWith(funcTemplate({
+          TEST: conditionalExpressionPath.node.test,
+          CONSEQUENT: conditionalExpressionPath.node.consequent,
+          ALTERNATE: conditionalExpressionPath.node.alternate,
+        }));
+      },
     },
   };
 }
