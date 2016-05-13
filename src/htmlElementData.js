e1e386a888902fc1ab240a42e864515dd21c01e3
Justin Silvestre <justinsilvestre@gmail.com>
2016-05-13 11:50:29 -0500

    Add c:out to list of void elements

--- src/htmlElementData.js
+++ src/htmlElementData.js
@@ -6,7 +6,9 @@ export const INLINE_ELEMENTS = ['b', 'big', 'i', 'small', 'tt',
 
 export const VOID_ELEMENTS = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img',
   'input', 'keygen', 'link', 'menuitem', 'meta', 'param', 'source',
-  'track', 'wbr'];
+  'track', 'wbr',
+  'c:out'];
 
 export const ATTRIBUTES_TO_TREAT_WITH_PROP_METHOD = ['async', 'autofocus', 'checked',
   'location', 'multiple', 'readOnly', 'selected'];
+  
